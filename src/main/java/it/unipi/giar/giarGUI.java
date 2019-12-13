4ed8286e89e60884d1f8b73dc3829c85083b9d27
seraogianluca <g.serao@studenti.unipi.it>
2019-12-13 17:12:20 +0100

    Create GUI class

---
+++ src/main/java/it/unipi/giar/giarGUI.java
@@ -0,0 +1,25 @@
+package it.unipi.giar;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class giarGUI extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		 Parent root = FXMLLoader.load(getClass().getResource("/fxml/SignUp.fxml"));
+	        
+		 primaryStage.setTitle("GIAR");
+		 primaryStage.setScene(new Scene(root));
+		 primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
