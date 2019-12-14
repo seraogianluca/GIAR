c58f27349816c43d16df8c67926394e4f460524c
seraogianluca <g.serao@studenti.unipi.it>
2019-12-14 10:31:33 +0100

    Update GiarGui

--- src/main/java/it/unipi/giar/GiarGui.java
+++ src/main/java/it/unipi/giar/GiarGui.java
@@ -8,7 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class giarGUI extends Application {
+public class GiarGui extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
