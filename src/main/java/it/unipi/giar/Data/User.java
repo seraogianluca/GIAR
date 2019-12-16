b57cbc24293d55ae40696e9996d388b280d317eb
seraogianluca <g.serao@studenti.unipi.it>
2019-12-16 17:59:02 +0100

    Register feature

--- src/main/java/it/unipi/giar/Data/User.java
+++ src/main/java/it/unipi/giar/Data/User.java
@@ -2,17 +2,51 @@ package it.unipi.giar.Data;
 
 import java.util.ArrayList;
 
-enum USERTYPE {admin, player, pro};
+import org.bson.Document;
+
+import com.mongodb.client.MongoCollection;
+
+import static com.mongodb.client.model.Filters.eq;
+
+import it.unipi.giar.MongoDriver;
 
 public class User {
-	private final long id;
-	private final USERTYPE type;
-	private final String nickname;
-	private final String email;
-	private final String password;
-	private final String country;
+	
+	private String type;
+	private String nickname;
+	private String email;
+	private String password;
+	private String country;
 	private ArrayList<Game> wishlist;
 	private ArrayList<Game> myGames;
 	private ArrayList<Rating> ratings;
+
+	public User(String type, String nickname, String email, String password, String country) {
+		this.type = type;
+		this.nickname = nickname;
+		this.email = email;
+		this.password = password;
+		this.country = country;
+	}
 	
+	public static boolean checkEmail(String email) {
+		MongoDriver md = MongoDriver.getInstance();
+		MongoCollection<Document> collection = md.getCollection("users");
+		Document user = collection.find(eq("email", email)).first();
+		if(user == null)
+			return true;
+		return false;	
+	}
+	
+	public void register() {
+		Document user = new Document("nickname", nickname)
+				.append("email", email)
+				.append("password", password)
+				.append("type", type)
+				.append("country", country);
+		
+		MongoDriver md = MongoDriver.getInstance();
+		MongoCollection<Document> collection = md.getCollection("users");
+		collection.insertOne(user);
+	}
 }
