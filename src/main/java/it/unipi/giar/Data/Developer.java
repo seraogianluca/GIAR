0d8673d50e61cc7c030294fc83f46d890784e8f3
mgomezgomez <marsha.gomez0906@gmail.com>
2019-12-19 11:20:24 +0100

    * Adding Developer, Genre and Platform Contructor.

--- src/main/java/it/unipi/giar/Data/Developer.java
+++ src/main/java/it/unipi/giar/Data/Developer.java
@@ -1,7 +1,41 @@
 package it.unipi.giar.Data;
 
+import org.bson.Document;
+
 public class Developer {
 	private final long id;
 	private String name;
 	private String slug;
+
+	public Developer(Document document) {
+		this.id = document.getLong("id");
+		this.name = document.getString("name");
+		this.slug = document.getString("slug");
+	}
+
+	public Developer(long id, String name, String slug) {
+		this.id = id;
+		this.name = name;
+		this.slug = slug;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSlug(String slug) {
+		this.slug = slug;
+	}
+
+	public long getId() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getSlug() {
+		return this.slug;
+	}
 }
